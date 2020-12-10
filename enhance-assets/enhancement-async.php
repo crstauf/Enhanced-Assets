e0659c1c14fd1606862dcc947b1869ae3f7dfc67
Caleb Stauffer <crstauf@users.noreply.github.com>
2020-12-10 13:20:56 -0500

    add support for async stylesheets

--- enhance-assets/enhancement-async.php
+++ enhance-assets/enhancement-async.php
@@ -13,16 +13,21 @@ class EnhanceAssets_AsyncEnhancement extends EnhanceAssets_Enhancement {
 	const KEY = 'async';
 
 	/**
-	 * Add "async" attribute to tag.
+	 * Add asynchronous functionality to tag.
 	 *
+	 * For scripts, add the "async" attribute.
+	 * For styles, change "media" attribute and add "onload" event.
+	 *
+	 * @link https://www.filamentgroup.com/lab/load-css-simpler/
 	 * @param string $tag
 	 * @return string
 	 */
 	protected function _enhance( string $tag ) {
-		if ( !$this->is_script )
-			return $tag;
-
-		return str_replace( '<script ', '<script async ', $tag );
+		if ( $this->is_script )
+			return str_replace( '<script ', '<script async ', $tag );
+			
+		$enhanced = str_replace( 'media=\'all\'', 'media=\'print\' onload="this.media=\'all\'"', $tag );
+		return $enhanced . "\n" . '<noscript>' . $tag . '</noscript>';
 	}
 
 }
